adf4635b2e77b493117bb3ef8453bf0a75bc6941
friedpies <kenmarut@gmail.com>
2019-05-30 19:44:07 -0500

    fixed portal

--- client/src/UrlScriptGenerator.js
+++ client/src/UrlScriptGenerator.js
@@ -3,21 +3,20 @@ import ReactDOM from "react-dom";
 import Axios from "axios";
 import App from "./components/App.jsx";
 
-const componentUrls = [
-  process.env.KEN_URL,
-  process.env.DAVID_URL
-];
+// const componentUrls = [process.env.KEN_URL, process.env.DAVID_URL];
 
-const componentUrlPromises = componentUrls.map(url => {
-  return Axios.get(url);
-});
+// const componentUrlPromises = componentUrls.map(url => {
+//   return Axios.get(url);
+// });
 
-Promise.all(componentUrlPromises)
-  .then(scripts => {
-    scripts.forEach(script => {
-      eval(script.data);
-    });
-  })
-  .then(() => {
-    ReactDOM.render(<App />, document.getElementById("app"));
-  });
+// Promise.all(componentUrlPromises)
+//   .then(scripts => {
+//     scripts.forEach(script => {
+//       eval(script.data);
+//     });
+//   })
+//   .then(() => {
+//     ReactDOM.render(<App />, document.getElementById("app"));
+//   });
+
+  ReactDOM.render(<App />, document.getElementById("app"));
--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,6 @@ const Dotenv = require("dotenv-webpack");
 
 module.exports = {
   entry: {
-    // renderer: `${SRC_DIR}/Renderer.jsx`,
-    // components: [`${SRC_DIR}/components/Title.jsx`, `${SRC_DIR}/components/SizeSelector.jsx`]
     app: `${SRC_DIR}/components/App.jsx`,
     urlScriptGenerator: `${SRC_DIR}/UrlScriptGenerator.js`
   },
